9abaf4a3f72c4c9e9bd463b47a73bdd225ee782e
Igor Zakhvatov <igor.zahvatov@gmail.com>
2023-06-05 22:57:49 +0300

    Added truncate string

---
+++ truncate_string.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var text string
+	var width int
+	fmt.Scanf("%s %d", &text, &width)
+
+	// Возьмите первые `width` байт строки `text`,
+	// допишите в конце `...` и сохраните результат
+	// в переменную `res`
+	// ...
+
+	res := ""
+
+	if len(text) > width {
+		res = string(([]byte(text))[:width]) + "..."
+	} else {
+		res = text
+	}
+
+	fmt.Println(res)
+}
